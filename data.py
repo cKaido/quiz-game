755c9147eefd625d44f1ab18bb53b03371d286f0
Cheung <cheung@tdfcu.com>
2022-10-06 16:01:34 -0400

    initial commit

---
+++ data.py
@@ -0,0 +1,22 @@
+question_data = [
+    {"category": "Animals", "type": "boolean", "difficulty": "easy",
+        "question": "Kangaroos keep food in their pouches next to their children.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "Minecraft can be played with a virtual reality headset.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "hard",
+        "question": "DHCP stands for Dynamic Host Configuration Port.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+        "question": "The song &quot;Stronger Than You&quot; is a single by Estelle, who played Garnet in Steven Universe.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "hard",
+        "question": "&quot;Cube&quot;, &quot;Cube 2: Hypercube&quot; and &quot;Cube Zero&quot; were directed by the same person.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy", "question": "In RuneScape, one must complete the &quot;Dragon Slayer&quot; quest before equipping Rune Platelegs.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In the Season One Championship of &quot;League of Legends&quot;, the highest achievable rank was Diamond.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+        "question": "In Chobits, Hideki found Chii in his apartment.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "medium",
+        "question": "In career mode of &quot;Need for Speed: Underground 2&quot;, the first car the player can drive is the BMW M3 GTR.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+        "question": "The mitochondria is the powerhouse of the cell.", "correct_answer": "True", "incorrect_answers": ["False"]}
+]
